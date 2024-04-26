a6b283c28f38f2f307072acd5da72d0d9de9cf11
mrfritzg <mrfritz@gamil.com>
2024-04-26 09:39:41 -0400

    update personal link

--- client/src/pages/MyJokes.jsx
+++ client/src/pages/MyJokes.jsx
@@ -1,5 +1,12 @@
-import { redirect, useLoaderData } from "react-router-dom";
+import {
+  redirect,
+  useLoaderData,
+  useNavigate,
+  useNavigation,
+} from "react-router-dom";
 import { jokesDBFetch } from "../utils/axiosfetchs";
+import { toast } from "react-toastify";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const loader = async () => {
   try {
@@ -16,38 +23,64 @@ export const loader = async () => {
   }
 };
 
+const DashboardContext = createContext();
+
 const MyJokes = () => {
   const { currentUserData, userJokes } = useLoaderData();
   // console.log(userJokes);
   const myJokes = userJokes?.jokes;
+
+  const navigate = useNavigate();
+
+  // adding navigation for Loading component spinner animation
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === "loading";
+
+  const logoutUser = async () => {
+    navigate("/");
+    await jokesDBFetch("/auth/logout");
+    toast.success("Logging Out...");
+  };
+
+
   return (
-    <main className="bg-white p-5 md:w-2/3 mx-auto">
-      <section>
-        <h1 className="font-bold text-center text-3xl my-5">
-          Welcome to My Jokes{" "}
-        </h1>
-        <p className="font-bold text-2xl my-4">
-          Hi {currentUserData?.user?.name}, Here are your Jokes
-        </p>
-        <div className="my-6">
-          <h1 className="text-xl my-2">Your Favorite Jokes</h1>
-          {myJokes.length > 0 ? (
-            myJokes.map((item) => (
-              <div className="jokeItem" key={item._id}>
-                {item.body}
+    <DashboardContext.Provider
+      value={{
+        currentUserData,
+        logoutUser,
+      }}
+    >
+      <main className="bg-white p-5 md:w-2/3 mx-auto">
+        <section>
+          <h1 className="font-bold text-center text-3xl my-5">
+            Welcome to My Jokes{" "}
+          </h1>
+          <p className="font-bold text-2xl my-4">
+            Hi {currentUserData?.user?.name}, Here are your Jokes
+          </p>
+          <div className="my-6">
+            <h1 className="text-xl my-2">Your Favorite Jokes</h1>
+            {myJokes.length > 0 ? (
+              myJokes.map((item) => (
+                <div className="jokeItem" key={item._id}>
+                  {item.body}
+                </div>
+              ))
+            ) : (
+              <div className="jokeItem text-red-600 font-bold text-xl">
+                Jokes Not Found...
               </div>
-            ))
-          ) : (
-            <div className="jokeItem text-red-600 font-bold text-xl">
-              Jokes Not Found...
-            </div>
-          )}
-        </div>
-        <div>
-          <h1 className="text-xl my-2">Your Created Jokes</h1>
-        </div>
-      </section>
-    </main>
+            )}
+          </div>
+          <div>
+            <h1 className="text-xl my-2">Your Created Jokes</h1>
+          </div>
+        </section>
+      </main>
+    </DashboardContext.Provider>
   );
 };
+// custom hook for the context for the data
+export const useDashboardContext = () => useContext(DashboardContext);
+
 export default MyJokes;
--- client/src/utils/links.jsx
+++ client/src/utils/links.jsx
@@ -1,8 +1,8 @@
 const links = [
-  { path: "/", name: "Home" },
-  { path: "/search", name: "Search" },
-  { path: "/about", name: "About" },
-  { path: "/login", name: "Login" },
+  { path: ".", name: "Home" },
+  { path: "search", name: "Search" },
+  { path: "about", name: "About" },
+  { path: "login", name: "Login" },
 ];
 
 export default links;
